75cd743b2d49b78b3a02071c54d682bd27ca3b51
Vadim Katkov <vmalyh777@gmail.com>
2024-10-10 17:55:34 +0300

    Разделить класс PostController на класс с бизнес-логикой и формированием ответа

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -4,55 +4,35 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
 
-import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
-import ru.yandex.practicum.catsgram.exception.NotFoundException;
 import ru.yandex.practicum.catsgram.model.Post;
 import ru.yandex.practicum.catsgram.marker.onCreate;
+import ru.yandex.practicum.catsgram.service.PostService;
 
-import java.time.Instant;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 @RestController
 @RequestMapping("/posts")
 public class PostController {
-    private final Map<Long, Post> posts = new HashMap<>();
+
+    PostService postService;
+
+    public PostController(PostService service) {
+        this.postService = service;
+    }
 
     @GetMapping
     public Collection<Post> findAll() {
-        return posts.values();
+        return postService.findAll();
     }
 
     @PostMapping
     public Post create(@Valid @RequestBody Post post) {
-        if(post.getDescription() == null || post.getDescription().isBlank()) {
-            throw new ConditionsNotMetException("Описание не может быть пустым");
-        }
-
-        post.setId(ControllerUtility.getNextId(posts.keySet()));
-        post.setPostDate(Instant.now());
-        posts.put(post.getId(), post);
-        return post;
+        return postService.create(post);
     }
 
     @PutMapping
     @Validated(onCreate.class)
     public Post update(@Valid @RequestBody Post newPost) {
-        if (newPost.getId() == null) {
-            throw new ConditionsNotMetException("Id должен быть указан");
-        }
-
-        if (posts.containsKey(newPost.getId())) {
-            Post oldPost = posts.get(newPost.getId());
-            if (newPost.getDescription() != null && !newPost.getDescription().isBlank()) {
-                oldPost.setDescription(newPost.getDescription());
-            }
-
-            return oldPost;
-        }
-
-        throw new NotFoundException("Пост с id = " + newPost.getId() + " не найден");
+        return postService.update(newPost);
     }
 }
