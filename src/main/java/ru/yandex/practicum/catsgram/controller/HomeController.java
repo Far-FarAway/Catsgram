e2be70aa69ea41268badd9ccd9650d0bf7ab7804
Vadim Katkov <vmalyh777@gmail.com>
2024-10-10 18:33:02 +0300

    Разделить класс на класс с бизнес-логикой и формированием ответа

--- src/main/java/ru/yandex/practicum/catsgram/controller/HomeController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/HomeController.java
@@ -1,15 +1,24 @@
 package ru.yandex.practicum.catsgram.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ru.yandex.practicum.catsgram.service.HomeService;
+
 @RestController
 @RequestMapping("/home")
 public class HomeController {
+    HomeService homeService;
+
+    @Autowired
+    public HomeController(HomeService service) {
+        homeService = service;
+    }
 
     @GetMapping
     public String homePage() {
-        return "<h1>Приветствуем вас, в приложении Котограм<h1>";
+        return homeService.homePage();
     }
 }
