620a5bdce33a14ee7d75d63a002883b080e153d9
Vadim Katkov <vmalyh777@gmail.com>
2024-10-10 18:46:11 +0300

    Разделить класс на класс с бизнес-логикой и формированием ответа

--- src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -4,86 +4,34 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.validation.annotation.Validated;
 import jakarta.validation.Valid;
 
-import ru.yandex.practicum.catsgram.exception.NotFoundException;
 import ru.yandex.practicum.catsgram.model.User;
-import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
-import ru.yandex.practicum.catsgram.exception.DuplicatedDataException;
 import ru.yandex.practicum.catsgram.marker.onCreate;
+import ru.yandex.practicum.catsgram.service.UserService;
 
-import java.time.Instant;
-import java.util.Map;
-import java.util.HashMap;
 import java.util.Collection;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    Map<Long, User> users = new HashMap<>();
+    UserService userService;
+
+    public UserController(UserService service) {
+        userService = service;
+    }
 
     @GetMapping
     public Collection<User> getUsers() {
-        return users.values();
+        return userService.getUsers();
     }
 
     @PostMapping
     public User postUser(@Valid @RequestBody User user) {
-        if (user.getEmail() == null || user.getEmail().isBlank()) {
-            throw new ConditionsNotMetException("Имейл должен быть указан");
-        } else {
-            if (ControllerUtility.isDuplicate(users, user.getEmail())) {
-                throw new DuplicatedDataException("Этот имейл уже используется");
-            }
-        }
-
-        if (user.getUsername() == null || user.getUsername().isBlank()) {
-            throw new ConditionsNotMetException("Имя не должно быть пустым");
-        } else {
-            if(ControllerUtility.isDuplicate(users, user.getUsername())) {
-                throw new DuplicatedDataException("Это имя уже занято");
-            }
-        }
-
-        if (user.getPassword() == null || user.getPassword().isBlank()) {
-            throw new ConditionsNotMetException("Пароль не должен быть пустой");
-        }
-
-        user.setId(ControllerUtility.getNextId(users.keySet()));
-        user.setRegistrationDate(Instant.now());
-        users.put(user.getId(), user);
-
-        return user;
+        return userService.postUser(user);
     }
 
     @PutMapping
     @Validated(onCreate.class)
     public User putUser(@Valid @RequestBody User user) {
-        if (user.getId() == null) {
-            throw new ConditionsNotMetException("Id должен быть указан");
-        }
-
-        if (users.containsKey(user.getId())) {
-            User oldUser = users.get(user.getId());
-            if (user.getEmail() != null && !(user.getEmail().isBlank())) {
-                if (ControllerUtility.isDuplicate(users, user.getEmail())) {
-                    throw new DuplicatedDataException("Этот имейл уже используется");
-                }
-                oldUser.setEmail(user.getEmail());
-            }
-
-            if (user.getUsername() != null && !(user.getUsername().isBlank())) {
-                if (ControllerUtility.isDuplicate(users, user.getUsername())) {
-                    throw new DuplicatedDataException("Это имя уже используется");
-                }
-                oldUser.setUsername(user.getUsername());
-            }
-
-            if (user.getPassword() != null) {
-                oldUser.setPassword(user.getPassword());
-            }
-
-            return oldUser;
-        }
-
-        throw new NotFoundException("Пользователь с ID " + user.getId() + " не найден");
+        return userService.putUser(user);
     }
 }
