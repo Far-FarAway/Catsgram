e7316806b731b580d84fa842230ab0eab89fe9a2
Tropicus <vmalyh777@gmail.com>
2024-11-22 22:34:43 +0300

    Убрать не использующийся импорт

--- src/main/java/ru/yandex/practicum/catsgram/Catsgram.java
+++ src/main/java/ru/yandex/practicum/catsgram/Catsgram.java
@@ -10,6 +10,7 @@ import java.util.Scanner;*/
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class Catsgram {
